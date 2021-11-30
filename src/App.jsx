3fadf33007630afabc451592bacf8ccc7794c738
Reuben George <reuben_george1998@outlook.com>
2021-11-30 21:10:25 +0000

    map-working-but-not-how-i-want-it-to

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import './App.scss';
+import team from "./data/team";
 
 import Header from './components/Header/Header';
-
-import EmployeeCard from "./components/EmployeeCard/EmployeeCard";
+import CardContainer from './components/CardContainer/CardContainer';
+import EmployeeCard from './components/EmployeeCard/EmployeeCard';
 
 function App() {
+  const names = team.map((person) => {
+    return person.name;
+  });
+  const roles = team.map((person) => {
+    return person.role;
+  });
+
+  console.log(names);
+  console.log(roles);
   return (
     <div className="App">
-      
       <Header />
-      <EmployeeCard employeeName="Reuben George" employeeRole="Junior Software Developer" />
+      {team.map((person) => {
+        {return <EmployeeCard employeeName={person.name} employeeRole={person.role}/>}
+      })}
     </div>
   );
 }
--- src/components/CardContainer/CardContainer.jsx
+++ src/components/CardContainer/CardContainer.jsx
@@ -1,2 +1,21 @@
 import React from "react";
-import "./TicketsSection.scss";
+import "./CardContainer.scss";
+import team from "../../data/team";
+
+import EmployeeCard from "../EmployeeCard/EmployeeCard";
+import EmployeeInfo from "../EmployeeInfo/EmployeeInfo";
+
+const CardContainter = () => {
+  const names = team.map((person) => {
+    return person.name;
+  });
+  const roles = team.map((person) => {
+    return person.role;
+  });
+
+  for (let i = 0; i < names.length; i++) {
+    return <EmployeeCard employeeName={names[i]} employeeRole={roles[i]} />;
+  }
+}
+
+export default CardContainter;
